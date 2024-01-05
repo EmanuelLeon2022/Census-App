8675456995052362781f769a9354be7ef54dc6da
jorge robayo <jorgerobmor@gmail.com>
2024-01-05 12:14:13 -0500

    basic folder structure

--- server.js
+++ server.js
@@ -1,35 +1,44 @@
 //Requirements
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 //mongo
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 //engine
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(methodOverride('_method'))
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(methodOverride("_method"));
 
 //----------{{Middleware}}
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
-mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
 
-app.use((req, res, next)=>{
-    console.log('I run for all routes')
-    next()
-})
+mongoose.set("strictQuery", false);
+
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
+});
 
 //----------{{Routes}}
 
-app.get('/', (req,res)=>{
-    res.send('<h1>Home Page</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
 
-app.listen(3000, (rq,res)=>{
-    console.log('Listenting to port 3000')
-})
+app.get("/home", (req, res) => {
+  res.render("Home");
+});
 
-
-
+app.listen(3000, (rq, res) => {
+  console.log("Listenting to port 3000");
+});
